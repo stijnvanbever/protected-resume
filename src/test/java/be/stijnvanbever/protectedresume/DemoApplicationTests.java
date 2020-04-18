3d53c4eafc6b4e218486271f9a02e306252dccdb
Stijn Van Bever <svanbever1@gmail.com>
2020-04-18 13:50:03 +0200

    Initial commit after Spring Initializr

---
+++ src/test/java/be/stijnvanbever/protectedresume/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.stijnvanbever.protectedresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
